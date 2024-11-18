c9e6cadf514f0a263846b250df19d280a940203b
Muneeb <muneeburrehman10997@gmail.com>
2024-11-19 01:46:14 +0500

    update changings

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,147 +12,149 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# # Internet Gateway
+# resource "aws_internet_gateway" "IGW" {
+#   vpc_id = aws_vpc.vpc.id
 
-# Internet Gateway
-resource "aws_internet_gateway" "IGW" {
-  vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "${var.identifier}-${var.IGW_name}-${terraform.workspace}"
+#   }
+# }
 
-  tags = {
-    Name = "${var.identifier}-${var.IGW_name}-${terraform.workspace}"
-  }
-}
+# # Public route table creation and adding internet gateway route
+# resource "aws_route_table" "public_RT" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "${var.identifier}-${var.public_RT_name}-${terraform.workspace}"
+#   }
+#   # route for internet gateway
+#   route {
+#     cidr_block = var.cidr_route_igw
+#     gateway_id = aws_internet_gateway.IGW.id
+#   }
+# }
 
+# Application route table creation 
+# resource "aws_route_table" "application_RT" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "${var.identifier}-${var.applicaiton_RT_name}-${terraform.workspace}"
+#   }
+# }
+
+# # data route table creation 
+# resource "aws_route_table" "data_RT" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "${var.identifier}-${var.data_RT_name}-${terraform.workspace}"
+#   }
+# }
 
 # Public Subnets creation 
 # public subnet 1 
-resource "aws_subnet" "public_subnet_1" {
+resource "aws_subnet" "subnets" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.public_subnet_1_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[0]
+  count      = "${length(var.subnets_cidr_block)}"
+  cidr_block = "${var.subnets_cidr_block[count.index]}"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
-    Name = "${var.identifier}-${var.public_subnet_1_name}-${terraform.workspace}"
+    Name = "${var.identifier}-${var.subnets_name[count.index]}-${terraform.workspace}"
   }
 }
-# public subnet 2
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.public_subnet_2_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[1]
-  tags = {
-    Name = "${var.identifier}-${var.public_subnet_2_name}-${terraform.workspace}"
-  }
-}
+# # public subnet 2
+# resource "aws_subnet" "public_subnet_2" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.public_subnet_2_cidr_block
+#   availability_zone = data.aws_availability_zones.available.names[1]
+#   tags = {
+#     Name = "${var.identifier}-${var.public_subnet_2_name}-${terraform.workspace}"
+#   }
+# }
 
 
 
-# Public Subnet Route Table with Subnet assosiation
-# Public route table creation and adding internet gateway route
-resource "aws_route_table" "public_RT" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.identifier}-${var.public_RT_name}-${terraform.workspace}"
-  }
-  # route for internet gateway
-  route {
-    cidr_block = var.cidr_route_igw
-    gateway_id = aws_internet_gateway.IGW.id
-  }
-}
+# # Public Subnet Route Table with Subnet assosiation
 
-# subnet assosiations (pub-subnet-1 , pub-subnet-2)
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_RT.id
-}
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_RT.id
-}
+# # subnet assosiations (pub-subnet-1 , pub-subnet-2)
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.public_subnet_1.id
+#   route_table_id = aws_route_table.public_RT.id
+# }
+
+# resource "aws_route_table_association" "b" {
+#   subnet_id      = aws_subnet.public_subnet_2.id
+#   route_table_id = aws_route_table.public_RT.id
+# }
 
 
 
-# Application Subnets creation 
-# application subnet 1 
-resource "aws_subnet" "application_subnet_1" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.application_subnet_1_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[0]
-  tags = {
-    Name = "${var.identifier}-${var.application_subnet_1_name}-${terraform.workspace}"
-  }
-}
-# application Subnet 2
-resource "aws_subnet" "application_subnet_2" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.application_subnet_2_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[1]
-  tags = {
-    Name = "${var.identifier}-${var.application_subnet_2_name}-${terraform.workspace}"
-  }
-}
+# # Application Subnets creation 
+# # application subnet 1 
+# resource "aws_subnet" "application_subnet_1" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.application_subnet_1_cidr_block
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   tags = {
+#     Name = "${var.identifier}-${var.application_subnet_1_name}-${terraform.workspace}"
+#   }
+# }
+# # application Subnet 2
+# resource "aws_subnet" "application_subnet_2" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.application_subnet_2_cidr_block
+#   availability_zone = data.aws_availability_zones.available.names[1]
+#   tags = {
+#     Name = "${var.identifier}-${var.application_subnet_2_name}-${terraform.workspace}"
+#   }
+# }
 
 
-# Application Subnet Route Table with Subnet assosiation
-# Application route table creation 
-resource "aws_route_table" "application_RT" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.identifier}-${var.applicaiton_RT_name}-${terraform.workspace}"
-  }
-}
-
-# subnet assosiations (app-subnet-1 , app-subnet-2)
-resource "aws_route_table_association" "c" {
+# # Application Subnet Route Table with Subnet assosiation
+# # subnet assosiations (app-subnet-1 , app-subnet-2)
+# resource "aws_route_table_association" "c" {
   
-  subnet_id      = aws_subnet.application_subnet_1.id
-  route_table_id = aws_route_table.application_RT.id
-}
+#   subnet_id      = aws_subnet.application_subnet_1.id
+#   route_table_id = aws_route_table.application_RT.id
+# }
 
-resource "aws_route_table_association" "d" {
-  subnet_id      = aws_subnet.application_subnet_2.id
-  route_table_id = aws_route_table.application_RT.id
-}
+# resource "aws_route_table_association" "d" {
+#   subnet_id      = aws_subnet.application_subnet_2.id
+#   route_table_id = aws_route_table.application_RT.id
+# }
 
 
 
-# data Subnets creation 
-# data subnet 1 
-resource "aws_subnet" "data_subnet_1" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.data_subnet_1_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[0]
-  tags = {
-    Name = "${var.identifier}-${var.data_subnet_1_name}-${terraform.workspace}"
-  }
-}
-# data Subnet 2
-resource "aws_subnet" "data_subnet_2" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.data_subnet_2_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[1]
-  tags = {
-    Name = "${var.identifier}-${var.data_subnet_2_name}-${terraform.workspace}"
-  }
-}
+# # data Subnets creation 
+# # data subnet 1 
+# resource "aws_subnet" "data_subnet_1" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.data_subnet_1_cidr_block
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   tags = {
+#     Name = "${var.identifier}-${var.data_subnet_1_name}-${terraform.workspace}"
+#   }
+# }
+# # data Subnet 2
+# resource "aws_subnet" "data_subnet_2" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.data_subnet_2_cidr_block
+#   availability_zone = data.aws_availability_zones.available.names[1]
+#   tags = {
+#     Name = "${var.identifier}-${var.data_subnet_2_name}-${terraform.workspace}"
+#   }
+# }
 
-# data Subnet Route Table with Subnet assosiation
-# data route table creation 
-resource "aws_route_table" "data_RT" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.identifier}-${var.data_RT_name}-${terraform.workspace}"
-  }
-}
-# subnet assosiations (data-subnet-1 , data-subnet-2)
-resource "aws_route_table_association" "e" {
-  subnet_id      = aws_subnet.data_subnet_1.id
-  route_table_id = aws_route_table.data_RT.id
-}
-resource "aws_route_table_association" "f" {
-  subnet_id      = aws_subnet.data_subnet_2.id
-  route_table_id = aws_route_table.data_RT.id
-}
+# # data Subnet Route Table with Subnet assosiation
+# # subnet assosiations (data-subnet-1 , data-subnet-2)
+# resource "aws_route_table_association" "e" {
+#   subnet_id      = aws_subnet.data_subnet_1.id
+#   route_table_id = aws_route_table.data_RT.id
+# }
+# resource "aws_route_table_association" "f" {
+#   subnet_id      = aws_subnet.data_subnet_2.id
+#   route_table_id = aws_route_table.data_RT.id
+# }
 
 
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -14,95 +14,98 @@ variable "vpc_name" {
     type = string
 }
 
-# internet gateway variables
-variable "IGW_name" {
-    description = "value for the internet_gateway name"
-    type = string
+# # internet gateway variables
+# variable "IGW_name" {
+#     description = "value for the internet_gateway name"
+#     type = string
+# }
+
+# variable "cidr_route_igw" {
+#     description = "value for the cidr_route_igw"
+#     type = string
+# }
+
+
+# subnet variables
+variable "subnets_cidr_block" {
+    description = "list for the  cidr_block"
+    type = list(string)
+    default = []
 }
 
-variable "cidr_route_igw" {
-    description = "value for the cidr_route_igw"
-    type = string
+variable "subnets_name" {
+    description = "list for subnets_name"
+    type = list(string)
+    default = []
+    
 }
 
+# variable "public_subnet_2_cidr_block" {
+#     description = "value for the public_subnet_1 cidr_block"
+#     type = string
+# }
 
-# public subnet variables
-variable "public_subnet_1_cidr_block" {
-    description = "value for the public_subnet_1 cidr_block"
-    type = string
-}
+# variable "public_subnet_2_name" {
+#     description = "value for the public-subnet-1 name"
+#     type = string
+# }
 
-variable "public_subnet_1_name" {
-    description = "value for the public-subnet-1 name"
-    type = string
-}
+# # application subnet variables
+# variable "application_subnet_1_cidr_block" {
+#     description = "value for the application_subnet_1 cidr_block"
+#     type = string
+# }
 
-variable "public_subnet_2_cidr_block" {
-    description = "value for the public_subnet_1 cidr_block"
-    type = string
-}
+# variable "application_subnet_1_name" {
+#     description = "value for the application-subnet-1 name"
+#     type = string
+# }
 
-variable "public_subnet_2_name" {
-    description = "value for the public-subnet-1 name"
-    type = string
-}
+# variable "application_subnet_2_cidr_block" {
+#     description = "value for the application_subnet_1 cidr_block"
+#     type = string
+# }
 
-# application subnet variables
-variable "application_subnet_1_cidr_block" {
-    description = "value for the application_subnet_1 cidr_block"
-    type = string
-}
+# variable "application_subnet_2_name" {
+#     description = "value for the application-subnet-1 name"
+#     type = string
+# }
 
-variable "application_subnet_1_name" {
-    description = "value for the application-subnet-1 name"
-    type = string
-}
+# # data subnet variables
+# variable "data_subnet_1_cidr_block" {
+#     description = "value for the data_subnet_1 cidr_block"
+#     type = string
+# }
 
-variable "application_subnet_2_cidr_block" {
-    description = "value for the application_subnet_1 cidr_block"
-    type = string
-}
+# variable "data_subnet_1_name" {
+#     description = "value for the data-subnet-1 name"
+#     type = string
+# }
 
-variable "application_subnet_2_name" {
-    description = "value for the application-subnet-1 name"
-    type = string
-}
+# variable "data_subnet_2_cidr_block" {
+#     description = "value for the data_subnet_1 cidr_block"
+#     type = string
+# }
 
-# data subnet variables
-variable "data_subnet_1_cidr_block" {
-    description = "value for the data_subnet_1 cidr_block"
-    type = string
-}
-
-variable "data_subnet_1_name" {
-    description = "value for the data-subnet-1 name"
-    type = string
-}
-
-variable "data_subnet_2_cidr_block" {
-    description = "value for the data_subnet_1 cidr_block"
-    type = string
-}
-
-variable "data_subnet_2_name" {
-    description = "value for the data-subnet-1 name"
-    type = string
-}
+# variable "data_subnet_2_name" {
+#     description = "value for the data-subnet-1 name"
+#     type = string
+# }
 
 # public route-table variables
-variable "public_RT_name" {
-    description = "value for the public_RT name"
-    type = string
-}
+# variable "public_RT_name" {
+#     description = "value for the public_RT name"
+#     type = string
+# }
 
-# application route-table variables
-variable "applicaiton_RT_name" {
-    description = "value for the data_RT name"
-    type = string
-}
+# # application route-table variables
+# variable "applicaiton_RT_name" {
+#     description = "value for the data_RT name"
+#     type = string
+# }
 
-# application route-table variables
-variable "data_RT_name" {
-    description = "value for the data_RT name"
-    type = string
-}
+# # application route-table variables
+# variable "data_RT_name" {
+#     description = "value for the data_RT name"
+#     type = string
+# }
