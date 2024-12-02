e8522fe495ae019041937fdcf14dbd30819579e5
muneeb10997 <your_email@example.com>
2024-12-02 13:59:40 +0500

    configration files added

--- application_ec2_user_data.sh
+++ application_ec2_user_data.sh
@@ -2,11 +2,13 @@
 
 # Update package list and install Python3 and pip
 sudo apt-get update -y
-sudo pip3 install --break-system-packages flask flask-mysqldb flask-cors
-
+sudo apt install -y python3-pip
+sudo apt install -y python3-flask
 # Install necessary Python packages globally (no virtual environment)
-sudo apt install python3-flask
-
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y pkg-config libmysqlclient-dev
+sudo pip3 install --break-system-packages flask flask-cors flask-mysqldb
+ 
 # Clone your Flask app from GitHub
 git clone https://github.com/muneeb10997/backend.git /home/ubuntu/backend
 
